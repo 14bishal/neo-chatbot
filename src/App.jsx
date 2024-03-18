b6ef5f29b57a0f59be1b7c7c889153fc14a8e037
Bishal Kumar Saha <64361639+14bishal@users.noreply.github.com>
2024-03-18 11:54:56 +0530

    fix: add the endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,99 +1,82 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
-
-import Footer from './components/Footer';
 
 import './App.css';
 
 function App() {
-  const [file, setFile] = useState(null);
-  const [inputValue, setInputValue] = useState('');
+    const [file, setFile] = useState(null);
+    const [inputValue, setInputValue] = useState('');
 
-  const onFileChangeHandler = (e) => {
-    e.preventDefault();
-    setFile(e.target.files[0]);
-  };
+    const onFileChangeHandler = (e) => {
+        e.preventDefault();
+        console.log('e.target.files', e.target.files)
+        setFile(e.target.files[0]);
+    };
 
-  // const handleSubmitFile = async () => {
-  //   const formData = new FormData();
-  //   formData.append('file', file);
-  //   console.log('formData', formData);
 
-  //   try {
-  //     const response = await axios.post('YOUR_API_ENDPOINT', formData, {
-  //       headers: {
-  //         'Content-Type': 'multipart/form-data',
-  //       },
-  //       responseType: 'arraybuffer',
-  //     });
+    const formSubmit = (e) => {
+        e.preventDefault();
+        const url = 'http://127.0.0.1:5000/buildJunits';  // change the end point as per the requirement
 
-  //     // Handle response
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error('Error uploading file:', error);
-  //   }
-  // };
+        axios.post(url, {repo_url: inputValue}).then(response => {
+            console.log('Response:', response.data);
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          })
 
-  // const handleSubmit = () => {
-  //   if (file) {
-  //     handleSubmitFile();
-  //     return;
-  //   }
-  // };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!file) {
-      console.error('No file selected');
-      return;
     }
 
-    const formData = new FormData();
-    formData.append('file', file);
+    return (
+        <div className="container">
+            <div className="title">Neo ChatBot</div>
+            <div className="sub-title">How can I help you today?</div>
+            <div className="bottom-section">
+                <form>
+                    <input
+                        type="file"
+                        className="form-control"
+                        name="file"
+                        id="select-file"
+                        onChange={onFileChangeHandler}
+                        style={{ display: "none" }}
+                    // accept=".zip"
+                    />
+                </form>
+                {file ? <div className="file-name">{file?.name}</div> : null}
+                <div className="footer-section">
+                    <textarea
+                        type="text"
+                        placeholder="Enter your code..."
+                        rows="1"
+                        className="text-area"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                    />
 
-    console.log('formData', formData);
+                    <label htmlFor="select-file">
+                        <div style={{ cursor: 'pointer' }}>
+                            <img
+                                width="26"
+                                height="26"
+                                src="https://img.icons8.com/3d-fluency/94/upload.png"
+                                alt="upload"
+                            />
+                        </div>
+                    </label>
 
-    try {
-      const response = await axios.post('YOUR_API_ENDPOINT', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data', // Use multipart/form-data for file uploads
-        },
-        responseType: 'arraybuffer',
-      });
-
-      // Handle response
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error uploading file:', error);
-    }
-  };
-
-  console.log('file', file, typeof file);
-
-  return (
-    <div className="container">
-      <div className="title">Neo ChatBot</div>
-      <div className="sub-title">How can I help you today?</div>
-      <div className="bottom-section">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="file"
-            className="form-control"
-            name="file"
-            onChange={onFileChangeHandler}
-            accept=".zip"
-          />
-        </form>
-        {/* {file ? <div className="file-name">{file?.name}</div> : null} */}
-        <Footer
-          handleSubmit={handleSubmit}
-          inputValue={inputValue}
-          setInputValue={setInputValue}
-        />
-      </div>
-    </div>
-  );
+                    <button type='submit' style={{ cursor: 'pointer' }} onClick={formSubmit}>
+                        <img
+                            width="26"
+                            height="26"
+                            src="https://img.icons8.com/arcade/64/sent.png"
+                            alt="sent"
+                        />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
